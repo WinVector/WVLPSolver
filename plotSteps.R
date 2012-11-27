6d9271091c3e20b51224a7557290ecd8793971e0
John Mount <jmount@win-vector.com>
2012-11-26 16:17:01 -0800

    update plots

--- plotSteps.R
+++ plotSteps.R
@@ -6,7 +6,10 @@ d2 <- melt(d,id.vars=c('assignmentSize'))
 ggplot(data=d2,aes(x=assignmentSize,y=value,color=variable)) + geom_point(alpha=0.5) + geom_smooth(alpha=0.7)
 ggsave('assignmentSpeed.png',width=8,height=6,dpi=90)
 d2$value = pmax(1,d2$value)
-ggplot(data=d2,aes(x=assignmentSize,y=value,color=variable)) + geom_point(alpha=0.5) + geom_smooth(alpha=0.7) +  scale_y_log10()
-summary(lm(log(value) ~ 0 + variable + variable:log(assignmentSize),data=d2))
+ggplot(data=d2,aes(x=assignmentSize,y=value,color=variable)) + geom_point(alpha=0.5) + geom_smooth(alpha=0.7) +  scale_y_log10() + scale_x_log10()
 ggsave('assignmentLogSpeed.png',width=8,height=6,dpi=90)
+summary(lm(log(value) ~ 0 + variable + variable:log(assignmentSize),data=subset(d2,d2$assignmentSize>40)))
+
+
+
 
