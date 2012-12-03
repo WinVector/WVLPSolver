38df579ac91dbf092c5a92102ce4a90860afd834
John Mount <jmount@win-vector.com>
2012-12-02 17:03:22 -0800

    turn off perturm RHS

--- src/com/winvector/lp/impl/RevisedSimplexSolver.java
+++ src/com/winvector/lp/impl/RevisedSimplexSolver.java
@@ -22,7 +22,7 @@ public final class RevisedSimplexSolver extends LPSolverImpl {
 	public double enteringTol = 1.0e-5;
 	public double leavingTol = 1.0e-7;
 	public boolean perturbC = true;
-	public boolean perturbB = true;
+	public boolean perturbB = false;               // this does change which bases are co-incident and which are feasible
 	public boolean earlyR = false;                 // allow partial inspection for entering columns
 	public boolean earlyLeavingCalc = true;       // value and sort steps early
 	public boolean earlyLeavingExit = true;       // allow early inspection exit on valuation calc
